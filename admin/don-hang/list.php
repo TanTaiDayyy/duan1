2e906f0d7719b839bd16f464655b7409bead3fd7
PhucThinhTr03 <thinhgoodvip263@gmail.com>
2022-12-14 13:16:42 +0700

    update 14/12

--- admin/don-hang/list.php
+++ admin/don-hang/list.php
@@ -7,129 +7,64 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?= $CONTENT_URL ?>/css/hoadon-admin.css">
     <title>Document</title>
-    <!-- <style>
-        .heading {
-            height: 36px;
+    <style>
+        .prod_item {
+            list-style: none;
         }
-        .prod-list {
-            
-            overflow-y: auto;
-        }
-        form.update-status-form {
-            flex: 0 0 100%;
-            max-width: 100%;
-            padding: unset;
-            margin: unset;
-        }
-
-       
-        .row-heading li{
-            background-color: black;
-            color:white;
-            padding: 5px;
-           
-            
-        }
-        .title{
-            padding-left: 550px;
-        }
-        
-       .id{
-        margin-left: 59px;
-       }
-       .qty span{
-        padding-right: 80px;
-       }
-      .status{
-        margin-top: 20px;
-        margin-left: 40px;
-      }
-      .prod_item li i{
-            color: black;
-            font-size: 25px;
-            margin-left: 30px;
-        }
-    </style> -->
+    </style>
 </head>
 
 <body>
     <div class="row title">
-        <h1>Receipt</h1>
+        <h1>RECEIPT OF ORDER</h1>
     </div>
     <div class="row prod-list">
-        <ul class="prod_item row-heading">
-            <li class="heading">ID</li>
-            <li class="heading">USER</li>
-            <li class="heading">Products</li>
-            <li class="heading">Total</li>
-            <li class="heading">Status</li>
-            <li class="heading"></li>
-        </ul>
         <?php
         $items = hoa_don_select_all();
         foreach ($items as $item) {
             extract($item)
         ?>
             <form action="" class="update-status-form">
-                <input type="hidden" name="ma_kh" value="<?= $ma_kh ?>">
-                <input type="hidden" name="ma_hd" value="<?= $ma_hd ?>">
-                <input type="hidden" name="ho_ten" value="<?= $ho_ten ?>">
-                <input type="hidden" name="dia_chi" value="<?= $dia_chi ?>">
-                <input type="hidden" name="so_dien_thoai" value="<?= $so_dien_thoai ?>">
-                <input type="hidden" name="total" value="<?= $total ?>">
-                <input type="hidden" name="ghi_chu" value="<?= $ghi_chu ?>">
-                <input type="hidden" name="ngay_tao" value="<?= $ngay_tao ?>">
-                <ul class=" prod_item ">
+                <input type="hidden" id="ma_kh" name="ma_kh" value="<?= $ma_kh ?>"><input type="hidden" id="ma_hd" name="ma_hd" value="<?= $ma_hd ?>"><input type="hidden" id="ho_ten" name="ho_ten" value="<?= $ho_ten ?>"><input type="hidden" id="dia_chi" name="dia_chi" value="<?= $dia_chi ?>"><input type="hidden" id="so_dien_thoai" name="so_dien_thoai" value="<?= $so_dien_thoai ?>"><input type="hidden" id="total" name="total" value="<?= $total ?>"><input type="hidden" id="ghi_chu" name="ghi_chu" value="<?= $ghi_chu ?>"><input type="hidden" id="ngay_tao" name="ngay_tao" value="<?= $ngay_tao ?>">
+                <div class=" prod_item ">
                     <li class="id"><?= $ma_hd ?></li>
-                    <li class="user-info">
-                        <ul>
-                            <li><?= $ho_ten ?></li>
-                            <li><?= $so_dien_thoai ?></li>
-                            <li><?= $dia_chi ?></li>
-                        </ul>
-
-                    </li>
+                    <li class="user-info"><?= $ho_ten ?></li>
+                    <li class="user-info"><?= $so_dien_thoai ?></li>
+                    <li class="user-info"><?= $dia_chi ?></li>
                     <li class="products">
-                        <ul>
-                            <?php
-                            $listSP = runSQL("SELECT hoa_don_chi_tiet.don_gia, hoa_don_chi_tiet.so_luong, hoa_don_chi_tiet.ma_hh, hang_hoa.ten_hh, hang_hoa.giam_gia FROM hoa_don_chi_tiet JOIN hang_hoa on hoa_don_chi_tiet.ma_hh = hang_hoa.ma_hh WHERE ma_hd=" . $ma_hd);
-                            foreach ($listSP as $x) : ?>
+                                    <?php $listSP = runSQL("SELECT hoa_don_chi_tiet.don_gia, hoa_don_chi_tiet.so_luong, hoa_don_chi_tiet.ma_hh, hang_hoa.ten_hh, hang_hoa.giam_gia FROM hoa_don_chi_tiet JOIN hang_hoa on hoa_don_chi_tiet.ma_hh = hang_hoa.ma_hh WHERE ma_hd=" . $ma_hd);
+                                    foreach ($listSP as $x) : ?>
                                 <li>
                                     <div class="receipt-item">
                                         <div class="info">
                                             <h4><a style="text-decoration: none;" href="../../site/trang-chinh/index.php?chi-tiet&ma_hh=<?= $x["ma_hh"] ?>" target="_blank" rel="noopener noreferrer"><?= $x['ten_hh'] ?></a>
-                                                <span style="font-weight: lighter; color: #555;">/</span> 
+                                                <span style="font-weight: lighter; color: #555;"></span>
                                             </h4>
                                             <div class="qty">
-                                                
                                                 <p class="subtotal">
-                                                    $<?= $x['don_gia'] ?>
+                                                    <?= $x['don_gia'] ?> VNĐ
                                                 </p>
                                                 <span>
-                                                    x<?= $x['so_luong'] ?>
+                                                    x <?= $x['so_luong'] ?>
                                                 </span>
                                             </div>
                                         </div>
-                                        
                                     </div>
-
                                 </li>
-                                
-                            <?php endforeach ?>
-                        </ul>
+                                    <?php endforeach ?>
                     </li>
-                    <li class="total">$<?= $total ?></li>
-                    <li class="status">
-                        <select name="trang_thai" id="">
-                            <option value="0" <?php echo ($trang_thai == 0) ? "selected" : "" ?>>Chưa Xác Nhận</option>
-                            <option value="1" <?php echo ($trang_thai == 1) ? "selected" : "" ?>>Đang xử lý</option>
-                            <option value="2" <?php echo ($trang_thai == 2) ? "selected" : "" ?>>Delivery</option>
-                            <option value="3" <?php echo ($trang_thai == 3) ? "selected" : "" ?>>Finished</option>
-                            <option value="4" <?php echo ($trang_thai == 4) ? "selected" : "" ?>>Cancel</option>
-                        </select>
-                    </li>
-                    <li class="action"><a href=""><i class="fa-regular fa-trash-can"></i></a></li>
-                </ul>
+                <li class="total"><?= $total ?> VNĐ</li>
+                <li class="status">
+                    <select name="trang_thai" id="">
+                        <option value="0" <?php echo ($trang_thai == 0) ? "selected" : "" ?>>Unconfirm</option>
+                        <option value="1" <?php echo ($trang_thai == 1) ? "selected" : "" ?>>Processing</option>
+                        <option value="2" <?php echo ($trang_thai == 2) ? "selected" : "" ?>>Delivery</option>
+                        <option value="3" <?php echo ($trang_thai == 3) ? "selected" : "" ?>>Finished</option>
+                        <option value="4" <?php echo ($trang_thai == 4) ? "selected" : "" ?>>Cancel</option>
+                    </select>
+                </li>
+                <li class="action"><a href=""><i class="fa-regular fa-trash-can"></i></a></li>
+                </div>
             </form>
         <?php } ?>
     </div>
